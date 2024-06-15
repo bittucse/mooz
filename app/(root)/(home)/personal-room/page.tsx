ae8c0fcce7d88262fff707fc9e6d78f64c096252
bittucse <bittucse@outlook.com>
2024-06-15 10:51:39 +0530

    build Sidebar

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Home = () => {
+const PersonalRoom = () => {
   return (
     <section className=' flex size-full flex-col gap-10 text-white'>
     <h1 className=' text-3xl font-bold'>
-      Home
+    PersonalRoom
     </h1>
     </section>
   )
 }
 
-export default Home
+export default PersonalRoom
--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Home = () => {
+const Previous = () => {
   return (
     <section className=' flex size-full flex-col gap-10 text-white'>
     <h1 className=' text-3xl font-bold'>
-      Home
+    Previous
     </h1>
     </section>
   )
 }
 
-export default Home
+export default Previous
