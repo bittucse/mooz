955ee7a30dab13f9646d695e28d3b384baaef2f1
bittucse <bittucse@outlook.com>
2024-06-19 20:34:05 +0530

    update Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,27 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className=' flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href="/" className='flex items-center gap-1'>
+      <Image 
+      src="/icons/logo.svg"
+      width={32}
+      height={32}
+      alt="mooz logo"
+      className=' max-sm:size-10'/>
+      <p className=' text-[26px] font-extrabold text-white max-sm:hidden'>Mooz</p>
+      </Link>
+      <div className='flex-between gap-5'>
+        {/* auth */}
+        <MobileNav />
+
+      </div>
       
-    </div>
+    </nav>
   )
 }
 
