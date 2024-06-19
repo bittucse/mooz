ae16ed47ee981be72328ea8803ce8463bbfe64c6
bittucse <bittucse@outlook.com>
2024-06-19 20:35:19 +0530

    update Mobilenav

---
+++ components/MobileNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MobileNav = () => {
+  return (
+    <section className=' w-full max-w-[264px]'>
+      MobileNav
+    </section>
+  )
+}
+
+export default MobileNav
