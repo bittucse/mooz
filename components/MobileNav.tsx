53d8d694f0bcb238733da8381c07595fe88a8d4e
bittucse <bittucse@outlook.com>
2024-06-20 20:28:34 +0530

    update UI

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from 'next/image'
+
 
 const MobileNav = () => {
   return (
     <section className=' w-full max-w-[264px]'>
-      MobileNav
+      <Sheet>
+        <SheetTrigger>
+          <Image src="/icons/hamburger.svg"
+          height={36}
+          width={36}
+          alt='hamburger icon' 
+          className=' cursor-pointer sm:hidden'/>
+
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+</Sheet>
+
     </section>
   )
 }
