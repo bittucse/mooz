506652d89c5e067229fea377291887085a9f512f
bittucse <bittucse@outlook.com>
2024-06-23 19:51:42 +0530

    update home UI

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,32 @@
+import MeetingTypeList from '@/components/MeetingTypeList';
 import React from 'react'
 
 const Home = () => {
+  const now=new Date();
+
+  const time=now.toLocaleTimeString('en-IN',{hour:'2-digit',minute:'2-digit', timeZone: 'Asia/Kolkata'});
+
+  const date=( new Intl.DateTimeFormat('en-IN',{dateStyle:'full'})).format(now);
   return (
     <section className=' flex size-full flex-col gap-10 text-white'>
-    <h1 className=' text-3xl font-bold'>
-      Home
-    </h1>
+    <div className=' h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+      <div className=' flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+        <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
+          Upcoming Meeting at : 12:30 PM
+        </h2>
+        <div className=' flex flex-col gap-2'>
+          <h1 className=' text-4xl font-extrabold  lg:text-7xl'>
+            {/* 11:30 AM */}
+            {time}
+          </h1>
+          <p className=' text-lg font-medium text-sky-1 lg:text-2xl'>
+            {/* Saturday , June 22, 2024 */}
+            {date}
+            </p>
+        </div>
+      </div>
+    </div>
+    <MeetingTypeList/>
     </section>
   )
 }
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react'
+
+
+const MeetingTypeList = () => {
+  return (
+    <section className=' grid grid-cols-1 gap-5
+    md:grid-cols-2 xl:grid-cols-4'>
+      <div className=' bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer' onClick={()=>{}}>
+        Box1
+      </div>
+    </section>
+  )
+}
+
+export default MeetingTypeList
