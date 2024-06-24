ef30d4cfb510d52cd5019b0c3ddcae58e067d4cd
bittucse <bittucse@outlook.com>
2024-06-24 09:18:10 +0530

    create reusable modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,19 +4,25 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
 
 
 const MeetingTypeList = () => {
 
   const router=useRouter();
 
-  const [meetingState,setMeetingState]=useState<'isScheduleMeeting' | 'isJoiningMeeting' |' isInstantMeeting'| undefined>()
+  const [meetingState,setMeetingState]=useState<'isScheduleMeeting' | 'isJoiningMeeting' |'isInstantMeeting'| undefined>()
+
+
+  const createMeeting=()=>{
+
+  }
 
 
   return (
     <section className=' grid grid-cols-1 gap-5
     md:grid-cols-2 xl:grid-cols-4'>
-      <HomeCard img="/icons/add-meeting.svg" title="New Meeting" description="Start an instant meeting" handleClick={()=>{setMeetingState('isJoiningMeeting')}}
+      <HomeCard img="/icons/add-meeting.svg" title="New Meeting" description="Start an instant meeting" handleClick={()=>{setMeetingState('isInstantMeeting')}}
       className="bg-orange-1"/>
 
       <HomeCard img="/icons/schedule.svg" title="Schedule Meeting" description="plan your meeting" handleClick={()=>{setMeetingState('isScheduleMeeting')}}
@@ -30,6 +36,12 @@ const MeetingTypeList = () => {
       // handleClick={()=> router.push('/recordings')}
       
       className="bg-yellow-1"/>
+
+      <MeetingModal isOpen={meetingState === 'isInstantMeeting'} onClose={()=>{setMeetingState(undefined)}}
+      title="Start an Instant Meeting"
+      className="text-center"
+      buttonText="Start Meeting"
+      handleClick={createMeeting}/>
     </section>
   )
 }
