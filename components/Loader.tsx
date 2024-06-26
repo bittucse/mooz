092bce1f863e5a9e5fa3825605734c1eae5ee00b
bittucse <bittucse@outlook.com>
2024-06-26 21:12:13 +0530

    update env

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -5,7 +5,7 @@ const Loader = () => {
   return (
     <div className=' flex-center h-screen w-full'>
       <Image src='/icons/loading-circle.svg' alt="loading"
-      width={50} height={50}/>
+      width={60} height={60}/>
     </div>
   )
 }
