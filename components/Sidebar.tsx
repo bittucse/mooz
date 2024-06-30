e65aab1203d2f3c6099d77f16dd4eb623fcf09de
bittucse <bittucse@outlook.com>
2024-06-30 21:26:44 +0530

    fix sidebar error

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,6 @@ import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
-import path from 'path';
 import React from 'react'
 
 const Sidebar = () => {
