b997cade0eef7750c0f00fafdf9763c35daf0c1f
bittucse <bittucse@outlook.com>
2024-07-07 19:52:24 +0530

    add colorConfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,12 +22,16 @@ const config = {
         dark:{
           1:"#1C1F2E",
           2:"#161925",
+          3:"#252A41",
+          4:"#1E2757",
         },
         blue:{
           1:"#0E78F9",
         },
         sky:{
           1:"#C9DDFF",
+          2:"#ECF0FF",
+          3:"#F5FCFF",
         },
         orange: {
           1: '#FF742E',
