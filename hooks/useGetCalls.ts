0830bda292adab5e018d22a4fbe0babc8e402dab
bittucse <bittucse@outlook.com>
2024-07-09 19:40:11 +0530

    update upcoming

--- hooks/useGetCalls.ts
+++ hooks/useGetCalls.ts
@@ -1,6 +1,28 @@
-import { Call } from "@stream-io/video-react-sdk"
-import { useState } from "react"
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk"
+import { useEffect, useState } from "react"
 
 export const useGetCalls = () =>{
     const [calls,setCalls] = useState<Call[]>([]);
+    const [isLoading,setIsLoading]=useState(false);
+    const client= useStreamVideoClient();
+    const {user}=useUser();
+
+    useEffect(()=>{
+        const loadCalls = async()=>{
+            if(!client || !user?.id) return;
+            setIsLoading(true)
+
+            try {
+                const {calls} = await client.queryCalls();
+                
+            } catch (error) {
+                console.log(error);
+            }finally{
+                setIsLoading(false);
+            }
+        }
+
+        loadCalls();
+    },[client,user?.id])
 }
