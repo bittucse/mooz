beda7c511e8fdf081904ec640d4559c0af899b3c
bittucse <bittucse@outlook.com>
2024-07-14 21:24:15 +0530

    update CallList

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,13 +1,30 @@
 'use client'
 
 import { useGetCalls } from '@/hooks/useGetCalls'
+import { CallRecording } from '@stream-io/video-react-sdk';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 
 const CallList = ({type}:{type: 'ended' | 'upcoming' | 'recording'}) => {
 
   const {endedCalls,upcomingCalls,callRecordings,isLoading}= useGetCalls();
   const router=useRouter();
+  const [recordings, setRecordings] = useState<CallRecording[]>([])
+
+  const getCalls = ()=>{
+    switch (type) {
+      case 'ended':
+        return endedCalls
+      case 'recording':
+        return recordings;
+      case 'upcoming':
+        return upcomingCalls;
+    
+      default:
+        return [];
+    }
+  }
 
   return (
     <div>
